4d03d1d82bc7a80121d8980e0f4ba6670fa14445
Xavier Glab <ksaweryglab@gmail.com>
2015-02-02 00:14:37 +0000

    added session midleware

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,3 +1,10 @@
 exports.render = function(req, res){
-  res.send("Hello Worldhhh");
-}
+
+  if(req.session.lastVisit){
+    console.log("last visit: ", req.session.lastVisit);
+  }
+
+  req.session.lastVisit = new Date();
+
+  res.render('index', {title: "I am coming from controller"});
+};
