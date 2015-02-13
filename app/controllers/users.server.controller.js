e2c3eaea477213bc7553c6b3c724c08103d76da9
Xavier Glab <ksaweryglab@gmail.com>
2015-02-13 08:15:26 +0000

    added user RESTful functions for getting user by id and getting all users

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -11,3 +11,34 @@ exports.create = function(req, res, next){
      }
   });
 };
+
+exports.list = function(re, res, next){
+  User.find({}, function(err, users){
+    if(err){
+      return next(err);
+    }else{
+      res.json(users);
+    }
+  });
+};
+
+//retrieve only username and email if you use:
+//User.find({}, 'username email', function(err, users){
+
+exports.read = function(req, res){
+  res.json(req.user);
+};
+
+exports.userByID = function(req, res, next, id){
+  User.findOne({_id: id}, function(err, user){
+    if(err){
+      return next(err);
+    }else{
+      console.log('id ', id);
+      console.log('user ', user);
+      //res.user = user; orig
+      res.json(user);
+      next();
+    }
+  });
+};
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,5 +1,13 @@
 var users = require('../controllers/users.server.controller');
 
 module.exports = function(app){
-  app.route('/users').post(users.create);
+  app.route('/users')
+    .post(users.create)
+    .get(users.list);
+
+  app.route('/users/:userId')
+    .get(users.read);
+
+  app.param('userId',users.userByID);
 };
+
