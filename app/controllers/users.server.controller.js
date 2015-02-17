5d1fd327ebbcae68cfa5fd7805b1af6335dd1467
Xavier Glab <ksaweryglab@gmail.com>
2015-02-17 01:07:59 +0000

    added saveOAuthUserPofile function to users.server.controller

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -21,6 +21,40 @@ var getErrorMessage = function(err){
   return message;
 };
 
+exports.saveOAuthUserProfile = function(req, profile, done){
+  User.findOne({
+    provider: profile.provider,
+    providerId: profile.providerId
+  }, function(err, user){
+    if(err){
+      return done(err);
+    }else{
+      if(!user){
+        var possibleUsername = profile.username || ((profile.email) ? profile.email.split('@')[0] : '');
+
+        User.findUniqueUsername(possibleUsername, null, function(availableUsername){
+           profile.username = availableUsername;
+
+          user = new User(profile);
+
+          user.save(function(err){
+             if(err){
+               var message = _this.getErrorMessage(err);
+
+               req.flash('error', message);
+               return res.redirect('/signup');
+             }
+            return done(err, user);
+          });
+        });
+      }else{
+        return done(err, user);
+      }
+    }
+  });
+};
+
+
 exports.renderSignin = function(req, res, next){
   if(!req.user){
     res.render('signin', {
