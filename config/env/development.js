ffae75c94a2051c41570d0ce598fa0ccc17b7627
Xavier Glab <ksaweryglab@gmail.com>
2015-02-26 07:26:24 +0000

    fixed authentication

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,13 @@
 module.exports = {
   //development configuration options
   db: 'mongodb://localhost/mean',
-  sessionSecret: 'developmentSessionSecret'
+  sessionSecret: 'developmentSessionSecret',
+  facebook: {
+    clientID: '1054264157923447',
+    clientSecret: '428f464a86ac4707fe6886719dda53d9',
+    callbackURL: 'http://localhost:4000/oauth/facebook/callback'
+  }
 };
+
+//the callbackURL property will be passed to Facebook OAuth service,
+//which will redirect to that URL after the authentication process is over
--- config/passport.js
+++ config/passport.js
@@ -20,3 +20,9 @@ module.exports = function(){
   require('./strategies/local.js')();
 
 };
+
+// passport.serialize and passport.deserialize are used to define how passport
+// will handle the serialization. When a user is authenticated, passport
+// will save its _id property to the session
+// Later on when the user object is needed, Passport will use the
+// _id property to grab the user object from the database.
